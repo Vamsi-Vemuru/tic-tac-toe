5d44a301526806c2b769f48957b010f2138cc0d9
Vamsavardan Vemuru <vamsi0493@gmail.com>
2015-12-08 23:08:32 +0530

    Documented

--- tictactoe/ttt/admin.py
+++ tictactoe/ttt/admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-class RegAdmin(admin.ModelAdmin):
-	list_display = ['__unicode__','email']
-	class Meta:
-		model = Registration
-
-admin.site.register(Registration, RegAdmin)
--- tictactoe/ttt/models.py
+++ tictactoe/ttt/models.py
@@ -4,11 +4,3 @@ from django.db import models
 
 # Create your models here.
 
-class Registration(models.Model):
-	first_name = models.CharField(max_length=120,null=False)
-	last_name = models.CharField(max_length=120,null=False)
-	username = models.CharField(max_length=120,null=False,blank=True,default="")
-	password = models.CharField(max_length=10,blank=True,default="")
-	email = models.EmailField()
-	def __unicode__(self):
-		return self.first_name
